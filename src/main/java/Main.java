873d461a5314446512f87df84303b08396a73038
Sergey Khoroshkov <serega44015@gmail.com>
2021-12-02 23:28:44 +0300

    my thour

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ser");
-        String a = "Javaa";
-        String b = "Seerrgp";
+        int a = 5;
+        int b = 6;
+        System.out.println( a + b);
     }
 }
