d1a6706988def81b181dbc55f9cc4334b47de78c
Sergey Khoroshkov <serega44015@gmail.com>
2021-12-06 22:11:44 +0300

    test on merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        String s = "Spring fraemwork";
-        System.out.println(s);
+        String testBranch = "Тест на слияние веток";
+        ArrayList<String> s = new ArrayList<>();
+        for (int a = 0; a < 50; a++){
+            s.add(testBranch);
+        }
+        s.stream().forEach(q -> System.out.println(q));
     }
 }
