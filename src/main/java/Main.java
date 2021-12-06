7b2312b29ad7d338b17258b0952784f6444e1ab8
Sergey Khoroshkov <serega44015@gmail.com>
2021-12-06 21:48:18 +0300

    dev test1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 6;
-        System.out.println( a + b);
-
-        String a1 = "Javaa";
-        String b1 = "Seerrgp";
+        String s = "Spring fraemwork";
+        System.out.println(s);
     }
 }
