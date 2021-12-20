d2a0bcf79ad476a4da1f30d635bc4424ba221316
Sergey Khoroshkov <serega44015@gmail.com>
2021-12-20 22:49:03 +0300

    front

---
+++ src/main/java/main/controller/ApiGeneralController.java
@@ -0,0 +1,9 @@
+package main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ApiGeneralController {
+}
