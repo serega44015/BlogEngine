661406649e0c4843a51306da2d418be891a38f08
MARVEL <serega44015@gmail.com>
2022-04-13 23:00:18 +0300

    13.04

--- src/main/java/main/controller/ApiPostController.java
+++ src/main/java/main/controller/ApiPostController.java
@@ -2,6 +2,8 @@ package main.controller;
 
 import main.api.response.PostsResponse;
 import main.service.PostsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/post")
+
 public class ApiPostController {
 
     private final PostsService postsService;
@@ -17,13 +20,24 @@ public class ApiPostController {
         this.postsService = postsService;
     }
 
-    @GetMapping
-    private PostsResponse posts(
-            @RequestParam(value = "offset", defaultValue = "0") int offset,
-            @RequestParam(value = "limit", defaultValue = "10") int limit,
-            @RequestParam(value = "mode", defaultValue = "recept") String mode)
-    {
-        return postsService.getPosts(offset, limit, mode);
+//    @GetMapping
+//    private PostsResponse posts(
+//            @RequestParam(value = "offset", defaultValue = "0") int offset,
+//            @RequestParam(value = "limit", defaultValue = "10") int limit,
+//            @RequestParam(value = "mode", defaultValue = "recent") String mode)
+//    {
+//        ResponseEntity
+//        return postsService.getPosts(offset, limit, mode);
+//    }
+
+    @GetMapping("/post")
+    public ResponseEntity<PostsResponse> posts(
+            @RequestParam(defaultValue = "0", required = false) Integer offset,
+            @RequestParam(defaultValue = "10", required = false) Integer limit,
+            @RequestParam(defaultValue = "recent", required = false) String mode) {
+        //return new ResponseEntity<>(postsService.getPost(offset, limit, mode), HttpStatus.OK);
+        return new ResponseEntity<>(postsService.getPosts(offset, limit, mode), HttpStatus.OK);
     }
+
 
 }
