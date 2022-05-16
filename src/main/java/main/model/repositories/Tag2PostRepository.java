7616ab6d7a608622d1e2834fce99cba61f65277b
n44301 <serega44015@gmail.com>
2022-05-16 15:11:03 +0300

    del comment

--- src/main/java/main/model/repositories/Tag2PostRepository.java
+++ src/main/java/main/model/repositories/Tag2PostRepository.java
@@ -9,12 +9,9 @@ import java.util.Optional;
 
 public interface Tag2PostRepository extends JpaRepository<Tags2Post, Integer> {
 
-    //@Query(value = "SELECT COUNT(tgPost.postId) FROM Tags2Post tgPost INNER JOIN Post p ON p.id = tgPost.postId INNER JOIN  Tag tg ON tg.id = tgPost.tagId WHERE Tag.name = 'Java'")
-    //@Query(value = "SELECT COUNT(tgPost.postId) FROM Tags2Post tgPost INNER JOIN Post p ON p.id = tgPost.postId INNER JOIN  Tag tg ON tg.id = tgPost.tagId WHERE tg.name = 'Java' ")
     @Query(value = "SELECT COUNT(tgPost.postId) FROM Tags2Post tgPost INNER JOIN Post p ON p.id = tgPost.postId INNER JOIN  Tag tg ON tg.id = tgPost.tagId WHERE tg.name = :tagName ")
     Integer countOfPostsWithTheName(@Param("tagName") String tagName);
-    //Optional<Tags2Post> findByName();
-    //Integer countOfPostsWithTheName();
+
 
 
 
